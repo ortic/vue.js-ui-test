7e610c0d5722d9ed522e631097f2ed5759660b1c
Remo Laubacher <remo.laubacher@gmail.com>
2016-10-25 09:40:21 +0200

    initial check-in of quasar framework test

---
+++ quasar-test/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Don't drop "quasar-app" class -->
+  <div id="quasar-app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// Root component
+export default {}
+</script>
+
+<style>
+</style>
